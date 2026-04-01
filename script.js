c937900d32d49b2f7d95d48a1ba7927e8e00490b
Jean-Louis ERRANTE <clensx@gmail.com>
2026-04-01 17:00:29 +0200

    Cours

---
+++ script.js
@@ -0,0 +1,109 @@
+// // Variables
+// let maVar = "La valeur" // Valeur qui change
+// const maConst = "Autre valeur" // Ne changera jamais
+
+// // Structures conditionnelles
+// // if (condition et condition ou (condition et condition)){}
+// if(maVar == "titi"){
+//     console.log("C'est ok")
+// } else if(maVar == "La valeur") {
+//     console.log("On est bon")
+// } else {
+//     console.log("Tant pis")
+// }
+
+// switch(maVar){
+//     case "titi": 
+//         console.log("C'est ok")
+//     break;
+//     case "La valeur":
+//         console.log("On est bon")
+//     break;
+//     default:
+//         console.log("Tant pis")
+// }
+
+// let age = 22
+// if (age > 16 && age < 18){
+//     console.log("Pas encore majeur")
+// } else if (age > 18 && age < 30){
+//     console.log("C'est bon t'es majeur")
+// }
+
+// // Boucles
+//     // compteur; condition; incrémentation/décrémentation
+// for(let i = 0; i < 100; i = i + 2){
+//     console.log(`Nombres pair : ${i}`)
+// }
+
+// age = 25
+// let prenom = "Jean-Louis"
+// let nom = "Errante"
+
+// console.log("Bonjour je m'appelle "+prenom+" et mon nom de famille c'est "+nom+", j'ai "+age+" ans")
+
+// console.log("Bonjour je m'appelle "+prenom+" "+nom+", j'ai "+age+" ans")
+
+// console.log(`Bonjour je m'appelle ${prenom} ${nom}, j'ai ${age} ans.`)
+
+// // Boucle while
+// // Amorçage, la variable de boucle
+// let i = 1
+
+// while(i < 100){
+//     console.log(`Nombres impair : ${i}`)
+//     // Modification de la condition
+//     i += 2
+//     // i = i + 2 → équivalent à la ligne au dessus
+// }
+
+// let valeur = "huhu"
+
+// while(valeur != "ahah"){
+//     console.log("La valeur est différente !")
+
+//     valeur = "ahah"
+// }
+
+// // DRY = Don't Repeat Yourself
+// // Fonctions
+
+// console.log("Bonjour Jean-Louis")
+// console.log("Bonjour Gérard")
+// console.log("Bonjour Matthieu")
+
+// function DireBonjour(prenom){
+//     console.log(`Bonjour ${prenom}`)
+// }
+// DireBonjour("Jean-Louis")
+// DireBonjour("Gérard")
+
+// // Fonction fléchée
+// const sayHello = (prenom) => {
+//     console.log(`Bonjour ${prenom}`)
+// }
+// sayHello("Matthieu")
+
+const h1 = document.querySelector('h1')
+
+// h1.innerHTML = "Totoo <br/> dfghjk;"
+h1.textContent = "Titi <br/> dfghjk;"
+
+const monPremierArticle = `
+        <article>
+            <h2>TITRE</h2>
+            <div>
+                <div>
+                    <ul>
+                        <li>A</li>
+                        <li>B</li>
+                        <li>C</li>
+                        <li>D</li>
+                        <li>E</li>
+                    </ul>
+                </div>
+                <a href="#">Lien</a>
+            </div>
+        </article>
+`
+document.querySelector("body > section:nth-child(2)").insertAdjacentHTML("afterbegin", monPremierArticle)
